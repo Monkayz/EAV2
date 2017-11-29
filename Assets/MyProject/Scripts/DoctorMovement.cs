3725ffef15e4948f0ef31219dd2f5986b00a4691
Jose <negretejose93@gmail.com>
2017-11-29 09:32:17 -0800

    Did more stuff Made temp doctors move

--- Assets/MyProject/Scripts/DoctorMovement.cs
+++ Assets/MyProject/Scripts/DoctorMovement.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Invector.CharacterController;
 
 public class DoctorMovement : MonoBehaviour {
 
 	public GameObject[] obj;
+	public GameObject playerController;
 	private bool playerExit = false;
 	
 	// Update is called once per frame
 	void Update () {
-		while (playerExit == true) {
+		if (playerExit == true) {
 			for (int i = 0; i < obj.Length; i++) {
 				obj[i].transform.position += new Vector3 (0f, 0f, -.1f);
 			}
--- Assets/MyProject/Scripts/FloorDestroyer.cs
+++ Assets/MyProject/Scripts/FloorDestroyer.cs
@@ -6,8 +6,12 @@ public class FloorDestroyer : MonoBehaviour {
 
 	public GameObject obj1;
 	public GameObject obj2;
+	public Vector3 RotateAmount;
 	private bool playerNextToKey = false;
 
+	void Update(){
+		transform.Rotate (RotateAmount * Time.deltaTime);
+	}
 	void OnTriggerEnter(Collider collider){
 		if (collider.tag == "Player") {
 			Destroy (obj1);
